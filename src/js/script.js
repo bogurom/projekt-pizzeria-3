07140044bfd3d2a1bcac685d212ce6bea34aa2a6
Bogumila Romanowska <bogurom@gmail.com>
2019-04-11 12:32:39 +0200

    Add sendOrder() in class Cart

--- src/js/script.js
+++ src/js/script.js
@@ -433,6 +433,8 @@
       thisCart.dom.toggleTrigger = thisCart.dom.wrapper.querySelector(select.cart.toggleTrigger);
       thisCart.dom.productList = thisCart.dom.wrapper.querySelector(select.cart.productList);
       // console.log('thisCart.dom.productList:', thisCart.dom.productList);
+      thisCart.dom.form = thisCart.dom.wrapper.querySelector(select.cart.form);
+      console.log('thisCart.dom.form:', thisCart.dom.form);
 
       thisCart.renderTotalsKeys = ['totalNumber', 'totalPrice', 'subtotalPrice', 'deliveryFee'];
 
@@ -453,6 +455,10 @@
       });
       thisCart.dom.productList.addEventListener('remove', function(){
         thisCart.remove(event.detail.cartProduct);
+      });
+      thisCart.dom.form.addEventListener('submit', function(event){
+        event.preventDefault();
+        thisCart.sendOrder();
       });
     }
 
@@ -519,6 +525,31 @@
       console.log('cartProduct.dom.wrapper:', cartProduct.dom.wrapper);
       cartProduct.dom.wrapper.remove();
       thisCart.update();
+    }
+
+    sendOrder(){
+      const thisCart = this;
+      const url = settings.db.url + '/' + settings.db.order;
+
+      const payload = {
+        address: 'test',
+        totalPrice: thisCart.totalPrice,
+      };
+
+      const options = {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(payload),
+      };
+
+      fetch(url, options)
+        .then(function(response){
+          return response.json;
+        }).then(function(parsedResponse){
+          console.log('parsedResponse:', parsedResponse);
+        });
     }
   }
 
